2e1715d038338ec1c9441dc29c374122623e0fad
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2022-09-07 19:51:00 -0400

    added function to elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const textInputForLetters = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -7,6 +7,8 @@ const remainingGuessesSpan = document.querySelector(".remaining span");
 const guessedMessages = document.querySelector(".message");
 const hiddenPlayAgain = document.querySelector(".play-again hide")
 const word = "Magnolia";
+const guessedLetters = [];
+
 
 
 
@@ -21,9 +23,49 @@ const placeholderLetters = [];
 wordInProgress.innerText = placeholderLetters.join("");
 }
 
+placeholder(word);
+
 guessButton.addEventListener("click", function(e) {
     e.preventDefault();
-    const guess = textInputForLetters.value;
+   //Empty Message Paragraph
+    textInputForLetters.value = "";
+      // Let's grab what was entered in the input
+    const guess = textInputForLetters.value; 
+      // Let's make sure that it is a single letter
+   const goodGuess= validateInput(guess);
     console.log(guess);
+
+    if (goodGuess) {
+        //We've got a letter! Let's guess
+        makeGuess(guess);
+    }
     textInputForLetters.value = "";
 });
+
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input === 0) {
+        //is the input empty?
+        guessedMessages.innerText = "Please enter a letter";
+
+    } else if (input.length > 1) {
+        //did you enter more than one letter?
+        guessedMessages.innerText = "did you enter more than one letter?"
+    } else if (!input.match(acceptedLetter)) {
+        // Did you type a number, a special character or some other non letter thing?
+        guessedMessages.innerText = "Please enter a letter from A to Z.";
+      } else {
+        // We finally got a single letter, omg yay
+        return input;
+      }
+    };
+
+    const makeGuess = function (guess) {
+        guess = guess.toUpperCase();
+       if (guessedLetters.includes(guess)) {
+        guessedMessages.innerText = "You've already guessed that m8";
+       } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+       }   
+    };
